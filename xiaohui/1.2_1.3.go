7adb45ce60c277bbd94279f0f51eca98713edba6
yanping.tan <yanping.tan@bitmain.com>
2019-11-09 18:22:10 +0800

    add 1.2_1.3

---
+++ xiaohui/1.2_1.3.go
@@ -0,0 +1,27 @@
+package xiaohui
+
+// T(n) = 1+2+3+...+(n-1)+n = (n+1)*n/2 = 0.5*n^2+0.5n = O(n^2)  # 等差数列求和，高斯算法
+// S(n) = 1 = O(1)
+func FindSameNum1(arr []int) (val, key1, key2 int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[i] == arr[j] {
+				return arr[i], j, i
+			}
+		}
+	}
+	return
+}
+
+// T(n) = n = O(n)
+// S(n) = n = O(n)
+func FindSameNum2(arr []int) (val, key1, key2 int) {
+	exist := map[int]int{}
+	for i := 0; i < len(arr); i++ {
+		if _, ok := exist[arr[i]]; ok {
+			return arr[i], exist[arr[i]], i
+		}
+		exist[arr[i]] = i
+	}
+	return
+}
---
+++ xiaohui/1.2_1.3_test.go
@@ -0,0 +1,21 @@
+package xiaohui
+
+import (
+	"testing"
+)
+
+func Test_FindSameNum_All(t *testing.T) {
+
+	arr := []int{3, 1, 2, 5, 4, 9, 7, 2}
+
+	val, key1, key2 := FindSameNum1(arr)
+	if val != 2 || key1 != 2 || key2 != 7 {
+		t.Errorf("FindSameNum1 error, val: %d, key1: %d key2: %d", val, key1, key2)
+	}
+
+	val, key1, key2 = FindSameNum2(arr)
+	if val != 2 || key1 != 2 || key2 != 7 {
+		t.Errorf("FindSameNum2 error, val: %d, key1: %d key2: %d", val, key1, key2)
+	}
+
+}
