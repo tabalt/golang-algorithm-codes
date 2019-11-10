3868a0f220656fceffa664bfca1c9ba5edab1beb
yanping.tan <yanping.tan@bitmain.com>
2019-11-10 19:20:27 +0800

    add 2.2_singly_linked_list

---
+++ xiaohui/2.2_singly_linked_list.go
@@ -0,0 +1,95 @@
+package xiaohui
+
+type SinglyNode struct {
+	Data int
+	Next *SinglyNode
+}
+
+type SinglyLinkdedList struct {
+	pHead *SinglyNode
+	pLast *SinglyNode
+
+	Size int
+}
+
+func NewSinglyLinkdedList() *SinglyLinkdedList {
+	return &SinglyLinkdedList{
+		pHead: nil,
+		pLast: nil,
+		Size:  0,
+	}
+}
+
+// T(n) = n = O(n)
+// S(n) = 1 = O(1)
+func (sll *SinglyLinkdedList) Get(idx int) *SinglyNode {
+	if idx < 0 || idx >= sll.Size {
+		return nil
+	}
+
+	tmpNode := sll.pHead
+	for i := 0; i < idx; i++ {
+		tmpNode = tmpNode.Next
+	}
+
+	return tmpNode
+}
+
+// T(n) = 1 = O(1) 	// ignore possible lookup operations
+// S(n) = 1 = O(1)
+func (sll *SinglyLinkdedList) Insert(idx, val int) {
+	if idx < 0 || idx > sll.Size {
+		return
+	}
+
+	insertNode := &SinglyNode{
+		Data: val,
+	}
+
+	if sll.Size == 0 {
+		sll.pHead = insertNode
+		sll.pLast = insertNode
+	} else if idx == 0 {
+		insertNode.Next = sll.pHead
+		sll.pHead = insertNode
+	} else if idx == sll.Size {
+		sll.pLast.Next = insertNode
+		sll.pLast = insertNode
+	} else {
+		prevNode := sll.Get(idx - 1)
+		insertNode.Next = prevNode.Next
+		prevNode.Next = insertNode
+	}
+
+	sll.Size++
+}
+
+// T(n) = n = O(1) 	// ignore possible lookup operations
+// S(n) = 1 = O(1)
+func (sll *SinglyLinkdedList) Delete(idx int) *SinglyNode {
+	if idx < 0 || idx >= sll.Size {
+		return nil
+	}
+
+	var deletedNode *SinglyNode
+	if idx == 0 {
+		deletedNode = sll.pHead
+
+		sll.pHead = sll.pHead.Next
+	} else if idx == sll.Size-1 {
+		deletedNode = sll.pLast
+
+		prevNode := sll.Get(idx - 1)
+		prevNode.Next = nil
+		sll.pLast = prevNode
+	} else {
+		prevNode := sll.Get(idx - 1)
+		deletedNode = prevNode.Next
+
+		prevNode.Next = prevNode.Next.Next
+	}
+	sll.Size--
+
+	return deletedNode
+
+}
