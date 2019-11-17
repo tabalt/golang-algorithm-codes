605d6ca61027b4cd75af86a0ff565853f1e4d627
yanping.tan <yanping.tan@bitmain.com>
2019-11-17 17:05:50 +0800

    add 2.3_stack

---
+++ xiaohui/2.3_stack.go
@@ -0,0 +1,42 @@
+package xiaohui
+
+//Stack based on array storage
+
+type ArrayStack struct {
+	storage *Array
+}
+
+func NewArrayStack(n int) *ArrayStack {
+	return &ArrayStack{
+		NewArray(n),
+	}
+}
+
+func (as *ArrayStack) Push(val int) {
+	as.storage.Insert(as.storage.Size(), val)
+}
+
+func (as *ArrayStack) Pop() int {
+	return as.storage.Delete(as.storage.Size() - 1)
+}
+
+//Stack based on list storage
+
+type ListStack struct {
+	storage *SinglyLinkdedList
+}
+
+func NewListStack() *ListStack {
+	return &ListStack{
+		NewSinglyLinkdedList(),
+	}
+}
+
+func (ls *ListStack) Push(val int) {
+	ls.storage.Insert(ls.storage.Size(), val)
+}
+
+func (ls *ListStack) Pop() int {
+	node := ls.storage.Delete(ls.storage.Size() - 1)
+	return node.Data
+}
---
+++ xiaohui/2.3_stack_test.go
@@ -0,0 +1,54 @@
+package xiaohui
+
+import (
+	"testing"
+)
+
+var (
+	stackInputCases = [][]int{
+		{1, 2, 3},
+		{4, 5, 6, 7, 8},
+		{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{5, 3, 8, 2, 0, 3, 4, 6, 9, 21},
+	}
+)
+
+func Test_Array_Stack_All(t *testing.T) {
+
+	for _, inputs := range stackInputCases {
+		as := NewArrayStack(5)
+
+		for _, val := range inputs {
+			as.Push(val)
+		}
+
+		for i := len(inputs) - 1; i >= 0; i-- {
+			v := inputs[i]
+			val := as.Pop()
+			if v != val {
+				t.Errorf("pop array stack error, get: %d, expect: %d", val, v)
+			}
+		}
+	}
+
+}
+
+func Test_List_Stack_All(t *testing.T) {
+
+	for _, inputs := range stackInputCases {
+		ls := NewListStack()
+
+		for _, val := range inputs {
+			ls.Push(val)
+		}
+
+		for i := len(inputs) - 1; i >= 0; i-- {
+			v := inputs[i]
+			val := ls.Pop()
+			if v != val {
+				t.Errorf("pop list stack error, get: %d, expect: %d", val, v)
+			}
+		}
+	}
+
+}
