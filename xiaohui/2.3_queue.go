7fd58303e010a0b067cf42ced7f50c0bba85dfc2
yanping.tan <yanping.tan@bitmain.com>
2020-01-05 19:14:46 +0800

    add 2.3_queue

---
+++ xiaohui/2.3_queue.go
@@ -0,0 +1,42 @@
+package xiaohui
+
+//Queue based on array storage
+
+type ArrayQueue struct {
+	storage *Array
+}
+
+func NewArrayQueue(n int) *ArrayQueue {
+	return &ArrayQueue{
+		NewArray(n),
+	}
+}
+
+func (aq *ArrayQueue) Enqueue(val int) {
+	aq.storage.Insert(aq.storage.Size(), val)
+}
+
+func (aq *ArrayQueue) Dequeue() int {
+	return aq.storage.Delete(0)
+}
+
+//Queue based on list storage
+
+type ListQueue struct {
+	storage *SinglyLinkdedList
+}
+
+func NewListQueue() *ListQueue {
+	return &ListQueue{
+		NewSinglyLinkdedList(),
+	}
+}
+
+func (lq *ListQueue) Enqueue(val int) {
+	lq.storage.Insert(lq.storage.Size(), val)
+}
+
+func (lq *ListQueue) Dequeue() int {
+	node := lq.storage.Delete(0)
+	return node.Data
+}
---
+++ xiaohui/2.3_queue_test.go
@@ -0,0 +1,54 @@
+package xiaohui
+
+import (
+	"testing"
+)
+
+var (
+	queueInputCases = [][]int{
+		{1, 2, 3},
+		{4, 5, 6, 7, 8},
+		{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{5, 3, 8, 2, 0, 3, 4, 6, 9, 21},
+	}
+)
+
+func Test_Array_Queue_All(t *testing.T) {
+
+	for _, inputs := range queueInputCases {
+		aq := NewArrayQueue(5)
+
+		for _, val := range inputs {
+			aq.Enqueue(val)
+		}
+
+		for i := 0; i < len(inputs); i++ {
+			v := inputs[i]
+			val := aq.Dequeue()
+			if v != val {
+				t.Errorf("dequeue array queue error, get: %d, expect: %d", val, v)
+			}
+		}
+	}
+
+}
+
+func Test_List_Queue_All(t *testing.T) {
+
+	for _, inputs := range queueInputCases {
+		lq := NewListQueue()
+
+		for _, val := range inputs {
+			lq.Enqueue(val)
+		}
+
+		for i := 0; i < len(inputs); i++ {
+			v := inputs[i]
+			val := lq.Dequeue()
+			if v != val {
+				t.Errorf("dequeue list queue error, get: %d, expect: %d", val, v)
+			}
+		}
+	}
+
+}
